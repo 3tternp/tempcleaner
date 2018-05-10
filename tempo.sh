d508e33cd4f3af4195b56004f768d25a8ec0ecee
astra <basnetprem1234@gmail.com>
2018-05-10 14:32:28 +0545

    Update tempo.sh

--- tempo.sh
+++ tempo.sh
@@ -1,1 +1,23 @@
+#!/bin/bash 
+#This script will automatically delete temporary files
+echo "Are you sure to delete temporary f!l3 ?"
+echo "Enter your choice:"
+
+read count
+ 
+if [ $count = "Y" ]
+then
+rm -rf /tmp/*
+rm -rf /var/tmp/*
+find ~ -type f \( -name '*.swp' -o -name '*~' -o -name '*.bak' -o -name '.netrwhist' \) -delete
+set dir=~/tmp/vim-swap-files//,/var/tmp//
+rm -rf ~/.local/share/Trash/*
+
+echo "Hurrey Temporary file are cleared"
+exit
+elif [ $count = "N" ]
+then
+  echo "No Temporary files are deleted !!!!"
+exit
+fi
 
